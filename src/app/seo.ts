6c6a4e495a0e3950a57c31ffa55032e2559bf604
noumanq <noumanq@asicomp.com>
2019-08-22 15:06:09 +0500

    first commit

--- src/app/seo.ts
+++ src/app/seo.ts
@@ -8,3 +8,27 @@ export class SeoLoginResponse{
     AccessToken: string;
     TokenExpirationTime: string;
 }
+
+export class Supplier{
+    CompanyId: number;
+    AsiNumber: string;
+    CompanyName : string;
+    Address1 : string;
+    Address2 : string;
+    City : string;
+    StateCode : string;
+    PostalCode : string;
+    County : string;
+    CountryCode : string;
+    PhoneNumber : string;
+    EmailAddress : string;  
+    MemberTypeCode : string;
+    SupplierLevel: number;
+    Premier : string;
+    Replink : string;        
+    Mapped : string;
+    TLC : string;  
+    Spotlight : string;
+    API : string;
+    HasNotes : boolean;       
+}
