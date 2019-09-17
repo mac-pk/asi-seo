e2acb498e6bf9d4558029329e34b13a8d3213c39
Sarmad Ali <sarmad.ali@asicentral.com>
2019-09-17 20:29:40 +0500

    Email Supplier Model Added

--- src/app/modals/email-supplier-modal/email-supplier-modal.component.ts
+++ src/app/modals/email-supplier-modal/email-supplier-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { EmailSupplier } from 'src/app/shared/models/emailSupplier/EmailSupplier';
 
 @Component({
   selector: 'app-email-supplier-modal',
@@ -8,17 +9,24 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class EmailSupplierModalComponent implements OnInit {
   @Input() supplier;
+  emailSupplier: EmailSupplier;
 
   constructor(
     public modal: NgbActiveModal
   ) { }
 
   ngOnInit() {
-    //console.log(this.supplier);
+    if (this.supplier) {
+      this.emailSupplier = new EmailSupplier(this.supplier);
+    }
   }
 
   close() {
     this.modal.dismiss();
+  }
+
+  clearAll() {
+    this.emailSupplier.To = '';
   }
 
 }
